5b9a733a7ead640755ea94cb4206b9a67b77d985
Sylvia Makuch <sylvia@trimboli.io>
2023-03-22 21:31:50 -0600

    init hero

---
+++ app/hero.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+const PageContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+`;
+
+const TextContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+`;
+
+const Header =styled.h1`
+    font-family: Orbitron;
+    color: #FC8CF1;
+    size: 5rem;
+    text-align: center;
+`
+
+
+
