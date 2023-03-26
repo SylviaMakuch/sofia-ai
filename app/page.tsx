43f578ee93d1de11c07ed50ac490d317cf9fae0d
Sylvia Makuch <sylvia@trimboli.io>
2023-03-26 15:09:35 -0600

    comment out for future edit

--- app/page.tsx
+++ app/page.tsx
@@ -8,28 +8,28 @@ import About from '../components/About'
 import Features from '../components/Features'
 import { Controller } from 'react-spring';
 
-interface IParallax {
-  horizontal: boolean
-  busy: boolean
-  space: number
-  offset: number
-  current: number
-  controller: Controller<{ scroll: number }>
-  layers: Set<IParallaxLayer>
-  container: React.MutableRefObject<any>
-  content: React.MutableRefObject<any>
-  scrollTo(offset: number): void
-  update(): void
-  stop(): void
-}
+// interface IParallax {
+//   horizontal: boolean
+//   busy: boolean
+//   space: number
+//   offset: number
+//   current: number
+//   controller: Controller<{ scroll: number }>
+//   layers: Set<IParallaxLayer>
+//   container: React.MutableRefObject<any>
+//   content: React.MutableRefObject<any>
+//   scrollTo(offset: number): void
+//   update(): void
+//   stop(): void
+// }
 
-interface IParallaxLayer {
-  horizontal: boolean
-  sticky: StickyConfig
-  isSticky: boolean
-  setHeight(height: number, immediate?: boolean): void
-  setPosition(height: number, scrollTop: number, immediate?: boolean): void
-}
+// interface IParallaxLayer {
+//   horizontal: boolean
+//   sticky: StickyConfig
+//   isSticky: boolean
+//   setHeight(height: number, immediate?: boolean): void
+//   setPosition(height: number, scrollTop: number, immediate?: boolean): void
+// }
 
 export default function Home() {
   const parallax = useRef<IParallax>(null!)
@@ -38,13 +38,13 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <Parallax ref={parallax} pages={3}>
+      {/* <Parallax ref={parallax} pages={3}> */}
         <Hero />
-        <ParallaxLayer offset={0} speed={0} style={{ top: '180'}}>
+        {/* <ParallaxLayer offset={0} speed={0} style={{ top: '180'}}> */}
           <About />
           <Features />
-        </ParallaxLayer>
-      </Parallax>
+        {/* </ParallaxLayer>
+      </Parallax> */}
     </main>
   )
 }
